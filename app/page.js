8da8803d63010765f9d63d9f3a23a615ade3c336
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-20 16:07:32 -0800

    add icon

--- app/page.js
+++ app/page.js
@@ -3,17 +3,41 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className='min-h-screen bg-gradient-to-tr from-[#60072C]
-    via-[#120B2E] to-[#091498] px-8 md:px-14 lg:px-36 pb-10 pt-7'>
-      <button type="button" class="text-white rounded px-10 py-5 
-      bg-gradient-to-r from-green-400 to-blue-500 
-      hover:from-pink-500 hover:to-yellow-500">
-        Volunteer
-      </button>
-      <button type="button" class="text-white rounded px-10 py-5 
-      bg-gradient-to-r from-green-400 to-blue-500 
-      hover:from-pink-500 hover:to-yellow-500">
-        Get Help
-      </button>
+    via-[#120B2E] to-[#091498] flex justify-center'>
+      <div className="max-width-xl">
+        {/* Image */}
+        <div className="flex flex-col lg:flex-row text-white">
+          <div className="lg:w-1/2 lg:block hidden">
+            <p className="text-lg">Your text here...</p>
+          </div>
+
+          <div className="lg:w-1/2">
+            <Image
+              src="/blind.png"
+              className="w-full h-auto"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+
+          <div className="lg:hidden mt-4">
+            <p className="text-lg">Your text here...</p>
+          </div>
+        </div>
+
+
+        <div>
+          <button type="button" className="text-white rounded px-10 py-5 
+          bg-gradient-to-r from-pink-500 to-yellow-500 hover:brightness-50">
+            Get Help
+          </button>
+          <button type="button" className="text-white rounded px-10 py-5 
+          bg-gradient-to-r from-pink-500 to-yellow-500 hover:brightness-50">
+            Get Help
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
