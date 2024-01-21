a541bb1e4bd4b858fc1883bf5e7e45ad7b4db2b0
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-20 21:56:49 -0800

    add api

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import { NextApiHandler } from 'next';
+import NextAuth from "next-auth";
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import GoogleProvider from 'next-auth/providers/google';
+import prisma from '../../../prisma/prisma';
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      checks: ['none']
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.moderator = user.moderator
+      return session
+    }
+  },
+  secret: process.env.SECRET,
+};
